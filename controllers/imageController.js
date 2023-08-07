d0ab29cbfdc6dbde803de1f0aeea3ce8d5d3a78f
Sahej Jain <89766122+Greninja28@users.noreply.github.com>
2023-08-08 03:30:09 +0530

    updated the links

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream'
 import mongoose from 'mongoose'
 
-const url = 'http://localhost:8000'
+const url = 'https://whatsapp-api-88n1.onrender.com'
 
 
 let gfs, gridfsBucket
